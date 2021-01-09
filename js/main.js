34663cad7d733a268dfd0717e548678589495dee
damiencfs <damiencherfils@gmail.com>
2021-01-09 13:15:19 +0100

    changement des sections en page html

--- js/main.js
+++ js/main.js
@@ -1,16 +1,6 @@
 $(document).ready(function(){
+    document.oncontextmenu = function() {
+        return false;
+      }
 
-        $("#accueil").css('display','block');
-        $("#club").css('display','none');
-        $("#inscrire").css('display','none');
-        $("#contact").css('display','none');
-        $("#shop").css('display','none');
-
-    $("#buy").click(function(){
-        $("#accueil").css('display','none');
-        $("#club").css('display','none');
-        $("#inscrire").css('display','none');
-        $("#contact").css('display','none');
-        $("#shop").css('display','block');
-    })
 })
